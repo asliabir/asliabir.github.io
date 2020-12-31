9d7aaee5d71b7972ca3632872aa1b542159ca009
asliabir <abiruzzaman.molla@gmail.com>
2020-12-31 22:02:01 +0600

    fix display position

--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@ $(window).on("load",function(){
     $(".loader-wrapper").fadeOut("slow");
     $('html').css({ 'overflow': 'auto', 'height': 'auto', 'position': 'relative' });
     $('.home').css({ 'position': 'relative' });
+    $('.social-icons').css({ 'display': 'inline' });
   });
 
 
